a778d783200cfe46045a27af15f8ab709995b341
Mark van de Korput <mark.korput@gmail.com>
2017-05-12 09:21:32 +0200

    first test testing destruction of child nodes

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -2,14 +2,56 @@
 #include "ofxUnitTests.h"
 #include "ofxInterface.h"
 
+#include <functional>
+
 // macros to isolate tests within their own local scope
 #define TEST_START(x) {ofLog()<<"CASE: "<<#x;
 #define TEST_END }
 
+class CustomNode : public ofxInterface::Node {
+public:
+    typedef std::function<void()> DestructorFunc;
+
+    DestructorFunc destructorFunc;
+
+    CustomNode(){
+        this->destructorFunc = [](){
+            // does nothing by default
+        };
+    }
+
+    ~CustomNode(){
+        this->destructorFunc();
+    }
+};
+
 class ofApp: public ofxUnitTestsApp{
-
     void run(){
+        TEST_START(delete children on remove)
+            CustomNode *rootNode, *childA, *childB;
+            rootNode = new CustomNode();
+            childA = new CustomNode();
+            childB = new CustomNode();
 
+            vector<string> result;
+            rootNode->destructorFunc = [&result](){
+                result.push_back("Destruct-Root");
+            };
+
+            childA->destructorFunc = [&result](){
+                result.push_back("Destruct-A");
+            };
+
+            childB->destructorFunc = [&result](){
+                result.push_back("Destruct-B");
+            };
+
+            rootNode->addChild(childA);
+            rootNode->addChild(childB);
+            delete rootNode;
+
+            test_eq(ofJoinString(result, " -> "), "Destruct-Root -> Destruct-A -> Destruct-B", "");
+        TEST_END
     }
 };
 
